5c092c2407e07783db33a30b3f13dbdd5719a2a9
David Trotz <david.trotz@gmail.com>
2017-07-08 21:06:51 -0700

    Fix to actually get the data we wanted.

--- app/controllers/greenhouse_controller.rb
+++ app/controllers/greenhouse_controller.rb
@@ -153,31 +153,32 @@ class GreenhouseController < ApplicationController
     last_24_hours_timestamp = (Time.now - 86400).to_i
 
     # Greenhouse internal sensor
-    fio_items = Greenhouse::DataPoint.joins(:greenhouse_sensor_data).where(" timestamp >= ? AND gh_sensor_data.sensor_id = ?", last_24_hours_timestamp, 1).pluck(:timestamp, :humidity)
+    fio_items = Greenhouse::DataPoint.joins(:greenhouse_sensor_data).where(" timestamp >= ? AND gh_sensor_data.sensor_id = ?", last_24_hours_timestamp, 1).pluck(:timestamp, :temperature)
     if !fio_items.nil? && fio_items.count > 0
       current_hour_timestamp = fio_items.first[0]
-      current_hour_total_humids = 0
-      current_hour_humid_count = 0
+      current_hour_total_temps = 0
+      current_hour_temp_count = 0
       fio_avg_items = Array.new
       fio_items.each do |item|
         time = item[0]
         temp = item[1]
         if time > (current_hour_timestamp + 60*60)
-          if current_hour_humid_count > 0
+          if current_hour_temp_count > 0
             ts = (current_hour_timestamp + time)/2
             dt = DateTime.strptime(String(ts),'%s')
             to = dt.to_time
             hour = '%02d' % to.hour
-            avg_humidity = current_hour_total_humids.to_f/current_hour_humid_count.to_f
-            avg = [hour, avg_humidity]
+            avg_c = current_hour_total_temps.to_f/current_hour_temp_count.to_f
+            avg_f = convert_to_fahrenheit(avg_c)
+            avg = [hour, avg_f]
             fio_avg_items.push(avg)
             current_hour_timestamp = time
-            current_hour_total_humids = 0
-            current_hour_humid_count = 0
+            current_hour_total_temps = 0
+            current_hour_temp_count = 0
           end
         else
-          current_hour_total_humids += temp
-          current_hour_humid_count += 1
+          current_hour_total_temps += temp
+          current_hour_temp_count += 1
         end
 
       end
@@ -200,8 +201,9 @@ class GreenhouseController < ApplicationController
             dt = DateTime.strptime(String(ts),'%s')
             to = dt.to_time
             hour = '%02d' % to.hour
-            avg_humidity = current_hour_cumulative_soc_temps.to_f/current_hour_soc_temps_count.to_f
-            avg = [hour, avg_humidity]
+            avg_c = current_hour_cumulative_soc_temps.to_f/current_hour_soc_temps_count.to_f
+            avg_f = convert_to_fahrenheit(avg_c)
+            avg = [hour, avg_f]
             fio_avg_items.push(avg)
             current_hour_timestamp = time
             current_hour_cumulative_soc_temps = 0
@@ -213,7 +215,7 @@ class GreenhouseController < ApplicationController
         end
 
       end
-      retval.push({name: 'Soc Temp', data: fio_avg_items})
+      retval.push({name: 'CPU Temp', data: fio_avg_items})
     end
 
   end
