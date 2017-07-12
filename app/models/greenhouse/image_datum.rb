d5ccdb43fae86b54abb9a44f267ffb774dd4e448
David Trotz <david.trotz@gmail.com>
2017-07-11 20:56:43 -0700

    One more fix

--- app/models/greenhouse/image_datum.rb
+++ app/models/greenhouse/image_datum.rb
@@ -7,16 +7,16 @@ module Greenhouse
     self.table_name = 'gh_image_data'
 
     def self.most_recent_image_name
-      most_recent_record = Greenhouse::ImageDatum.order('gh_data_point_id').last
-      full_path = most_recent_record.filename
+      most_recent_data_point = Greenhouse::DataPoint.where(synchronized: 1).order('timestamp').last
+      image_data = most_recent_data_point.greenhouse_image_data.first!
+      full_path = image_data.filename
       local_path = 'imgs/2017/' + full_path.split('/').last
       return local_path
     end
 
     def self.most_recent_image_alt_text
-      most_recent_record = Greenhouse::ImageDatum.order('gh_data_point_id').last
-      gh_dp = Greenhouse::DataPoint.where(id: most_recent_record.gh_data_point_id).first!
-      alt_text = Date.strptime("#{gh_dp.timestamp}", "%s").strftime('%A, %B %_m, %Y at %l:%M %P')
+      most_recent_data_point = Greenhouse::DataPoint.where(synchronized: 1).order('timestamp').last
+      alt_text = Date.strptime("#{most_recent_data_point.timestamp}", "%s").strftime('%A, %B %_m, %Y at %l:%M %P')
       return alt_text
     end
   end
